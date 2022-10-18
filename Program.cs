cbc01061a5873dddbb8e6ec70640a0839a271f04
Andrea Burr <burrandy18@gmail.com>
2022-10-18 13:43:29 -0700

    Mini2

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿// Andrea Burr
+// 10-18-2022
+// mini2 redo - github
+Console.Clear();
+bool playagain = true;
+
+while (playagain){
+
+Console.WriteLine("lets add two numbers!");
+Console.WriteLine("enter your first number!");
+string numberOne = Console.ReadLine();
+Console.WriteLine("Enter your second number please");
+string numberTwo = Console.ReadLine();
+
+int num1 = Convert.ToInt32(numberOne);
+int num2 = Convert.ToInt32(numberTwo);
+
+Console.WriteLine("your sum is " + (num1 + num2) + " hope this helps!");
+Console.WriteLine("if you are done playing type no or press any key to continue");
+string contPlay = Console.ReadLine();
+
+  if (contPlay == "no"){
+
+ playagain = false; 
+  }else{
+    playagain = true;
+  }
+}
+
